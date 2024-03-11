097990ded94d73b970ef678fe37315e82bff80eb
Anna Burova <burova.anna83@mail.ru>
2024-03-11 14:54:49 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         MonthsOfRest service = new MonthsOfRest();
         System.out.println(service.monthsOfRest(100_000, 60_000, 150_000));
+        System.out.println(service.monthsOfRest(10_000, 3_000, 20_000));
     }
 }
